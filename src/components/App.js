827ec34511ed4df8aaa22bed3a19626a80db3fba
CapicheDesign <silentdesigns@Silents-iMac.local>
2020-06-25 19:09:13 +1200

    up to video 21

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,38 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base"; //import firebase
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    //first reinstate localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish = (fish) => {
     // Take a copy of existing state
     const fishes = { ...this.state.fishes };
@@ -20,22 +46,79 @@ class App extends React.Component {
       fishes,
     });
   };
+
+  updateFish = (key, updatedFish) => {
+    //take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // update that state
+    fishes[key] = updatedFish;
+    // set that to state
+    this.setState({ fishes });
+  };
+
+  deleteFish = (key) => {
+    // take a copy of state
+    const fishes = { ...this.state.fishes };
+    // update the state
+    fishes[key] = null;
+    // update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({
-      fishes: sampleFishes
+      fishes: sampleFishes,
     });
   };
+
+  addToOrder = (key) => {
+    //take a copy of state
+    const order = { ...this.state.order };
+    // either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // set new order object to state
+    this.setState({
+      order,
+    });
+  };
+
+  removeFromOrder = (key) => {
+    // take a copy of state
+    const order = { ...this.state.order };
+    // remove item from order
+    delete order[key];
+    // update state
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
+        <Inventory
+          fishes={this.state.fishes}
+          addFish={this.addFish}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { formatPrice } from '../helpers';
+import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
-    render() {
-        const {image, name, desc, price, status} = this.props.details;
-        return (
-           <li className="menu-fish">
-               <img src={image} alt={name}/>
-               <h3 className="fish-name">{name}
-                <span className="price">{ formatPrice(price) }</span>
-               </h3>    
-               <p>{desc}</p>
-               <button>Add to cart</button>
-           </li>
-        )
-    }
+  render() {
+    const { image, name, desc, price, status } = this.props.details;
+    const isAvailable = status === "available";
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(this.props.index)}
+        >
+          {isAvailable ? "Add to order" : "Sold out!"}
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Fish;
