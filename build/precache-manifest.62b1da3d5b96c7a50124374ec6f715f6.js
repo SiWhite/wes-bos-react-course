e5425fab1bcac74b84a578090e01fd7cd6103d5b
Simon <simon@capiche.co.nz>
2020-06-26 19:41:46 +1200

    Adding build folder

---
+++ build/precache-manifest.62b1da3d5b96c7a50124374ec6f715f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541ed3c8ed7ae053bc890931519feac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3975e1aee1f806b1fb82",
+    "url": "/static/css/main.564cfc51.chunk.css"
+  },
+  {
+    "revision": "87269dd9d38bc3ca700d",
+    "url": "/static/js/2.66bdeb2d.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.66bdeb2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3975e1aee1f806b1fb82",
+    "url": "/static/js/main.b73e94c1.chunk.js"
+  },
+  {
+    "revision": "682f9dc0a88c8bde3947",
+    "url": "/static/js/runtime-main.eb0b6bae.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
